ac64c05b0acdfab8f97079c16537becd84cc0cd6
hrlnsnchz <hrlnsnchz@gmail.com>
2021-01-28 21:42:48 -0600

    Added ability to send notes to API

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,13 @@ import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
 import { CriminalList } from "./criminals/CriminalList.js";
 import { OfficerSelect } from "./officers/OfficerSelect.js";
 import { OfficerList } from './officers/OfficerList.js'
+import { NoteForm } from "./notes/NoteForm.js";
+import { saveNote } from "./notes/NoteProvider.js";
 
-
+NoteForm()
 CriminalList()
 ConvictionSelect()
 OfficerSelect()
 OfficerList()
+// saveNote()
 
