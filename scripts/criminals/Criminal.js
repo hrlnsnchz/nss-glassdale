100c303c0009f9b800f76789fccbb8f246dcffed
hrlnsnchz <hrlnsnchz@gmail.com>
2021-02-14 13:00:16 -0600

    added facilities to corresponding criminals using IDs. Broke dropdown selection, I believe due to the change of the render function. Must fix later.

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,12 +1,19 @@
-export const Criminal = (criminalObj) => {
+export const Criminal = (criminalObj, facilitiesObj) => {
     return `
         <article class="criminals">
         <h4>${criminalObj.name}</h4>
-        <p> Age: ${criminalObj.age}</p>
+        <p>Age: ${criminalObj.age}</p>
         <p>Crime: ${criminalObj.conviction}</p>
+        <p>Arrested by: ${criminalObj.arrestingOfficer}</p>
         <p>Term start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p>
         <p>Term end: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p>
-        <button id="associates--${criminalObj.id}">Associate Alibis</button>
+        <div>
+                <h4>Facilities:</h4>
+                <ul>
+                    ${facilitiesObj.map(f => `<li>${f.facilityName}</li>`).join("")}
+                </ul>
+            </div>
+            <button id="associates--${criminalObj.id}">Show Associates</button>
         </article>
 `
 }
--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -3,31 +3,47 @@ import { Criminal } from './Criminal.js'
 import { useConvictions } from './../convictions/ConvictionProvider.js'
 // import { useAlibis } from '../alibis/AlibiProvider.js'
 import { Alibi } from '../alibis/Alibi.js'
+import { getFacilities, useFacilities } from '../facilities/FacilityProvider.js'
+import { getCriminalFacilities, useCriminalFacilities } from '../facilities/CriminalFacilityProvider.js'
 
-let criminalsContainer = document.querySelector(".criminalsContainer")
+let contentTarget = document.querySelector(".criminalsContainer")
 const eventHub = document.querySelector(".container")
 
-// renders the criminals when called
-const render = (criminalCollection) => {
-    let criminalsHTMLRepresentations = ""
-  
-    for (const criminal of criminalCollection) {
-      criminalsHTMLRepresentations += Criminal(criminal)
-    }
-    
-    criminalsContainer.innerHTML = `
-    <h3>Criminals</h3>
-    <section class="criminalsList">
-    ${criminalsHTMLRepresentations}
-    </section>`
-  }
+
+  const render = (criminalsToRender, allFacilities, allRelationships) => {
+    // Step 1 - Iterate all criminals
+    contentTarget.innerHTML = criminalsToRender.map(
+        (criminalObject) => {
+            // Step 2 - Filter all relationships to get only ones for this criminal
+            const facilityRelationshipsForThisCriminal = allRelationships.filter(cf => cf.criminalId === criminalObject.id)
+
+            // Step 3 - Convert the relationships to facilities with map()
+            const facilities = facilityRelationshipsForThisCriminal.map(cf => {
+                const matchingFacilityObject = allFacilities.find(facility => facility.id === cf.facilityId)
+                return matchingFacilityObject
+            })
+
+            // Must pass the matching facilities to the Criminal component
+            return Criminal(criminalObject, facilities)
+        }
+    ).join("")
+}
 
   export const CriminalList = () => {
-    getCriminals()
-        .then(() => {
-            const criminalsArray = useCriminals()
-            render(criminalsArray)
-        })
+    // Kick off the fetching of both collections of data
+    getFacilities()
+        .then(getCriminalFacilities)
+        .then(
+            () => {
+                // Pull in the data now that it has been fetched
+                const facilities = useFacilities()
+                const crimFac = useCriminalFacilities()
+                const criminals = useCriminals()
+
+                // Pass all three collections of data to render()
+                render(criminals, facilities, crimFac)
+            }
+        )
 }
 
 // Listen for the "crimeChosen" custom event you dispatched in ConvictionSelect
